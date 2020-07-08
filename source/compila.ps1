a53760feda47117bd387028ebffa35f25b2413e4
Tomeu Capó <tomeucapo@gmail.com>
2020-07-08 02:43:57 +0200

    Moved hex files to bin directory

--- source/compila.ps1
+++ source/compila.ps1
@@ -1,3 +1,3 @@
-zmac int32k.asm -o int32k.hex
-zmac bas32k.asm -o bas32k.hex
-srec_cat int32k.hex -Intel bas32k.hex -Intel -o rom.hex -Intel
+zmac int32k.asm -o bin/int32k.hex
+zmac bas32k.asm -o bin/bas32k.hex
+srec_cat bin/int32k.hex -Intel bin/bas32k.hex -Intel -o bin/rom.hex -Intel
